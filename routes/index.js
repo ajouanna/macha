0d0c3a3059e8dd66d1931d8c208d30b3e4dc8a2f
Antoine <antoine.jouannais@gmail.com>
2017-03-15 15:43:10 +0100

    passage de macha a matcha

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Projet Macha' });
+  res.render('index', { title: 'Projet Matcha' });
 });
 
 module.exports = router;
